33a1f2a7c7907b0bf8cda4798739a7e4152217a0
Guilherme Randon <guilherme.randon@gmail.com>
2020-09-07 18:44:09 -0300

    Added Seeding service.

--- SalesWebMVC1/Startup.cs
+++ SalesWebMVC1/Startup.cs
@@ -12,17 +12,22 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC1.Data;
 
-namespace SalesWebMVC1 {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace SalesWebMVC1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.Configure<CookiePolicyOptions>(options => {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
@@ -31,16 +36,24 @@ namespace SalesWebMVC1 {
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-    services.AddDbContext<SalesWebMVC1Context>(options =>
-            options.UseMySql(Configuration.GetConnectionString("SalesWebMVC1Context"), builder => builder.MigrationsAssembly("SalesWebMVC1")));
+            services.AddDbContext<SalesWebMVC1Context>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMVC1Context"), builder => builder.MigrationsAssembly("SalesWebMVC1")));
+
+            services.AddScoped<SeedingService>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
-            else {
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
@@ -49,7 +62,8 @@ namespace SalesWebMVC1 {
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes => {
+            app.UseMvc(routes =>
+            {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
